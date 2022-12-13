53f7b894fca04ccad796182571779b94b8671675
mvefagh <94897501+mvefagh@users.noreply.github.com>
2022-12-13 15:31:56 +0100

    index

---
+++ how_to_print.py
@@ -0,0 +1,47 @@
+
+# Title of the Program
+print("Willkomen zur Python Schulung")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print()    # Empty Line
+
+
+# Participant List
+print("Teilnehmerliste:")
+print("-" * 16)  # Repeate '-' 16x times
+print("1. Ingo")
+print("2. Thomas")
+print("3. Frank")
+
+
+# Topic List
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print()
+
+print("Themen:")
+print("-" * 8)
+print("1. Basics")
+print("2. Flows")
+print("3. Data Structure")
+print()
+
+#######################
+# Print ( Suffix )  - end (default) : New line
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print( "A"  )
+print("B", end = "---")
+print("C")
+print("D")
+print()
+
+########################
+# Join Several Values
+# ~~~~~~~~~~~~~~~~~~~~~
+# Wih comma, you can join diffenet texts
+# But it makes a "1x Space" between items
+
+print( "Python", "JAVA", "C#") # with space in between
+
+print( "Python"+ "JAVA"+ "C#") # without space in between
+
+print( "Apfel" + "Banana" ) # ApfelBanana
+
